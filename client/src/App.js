7111d44d0eafacdbc32ae7d7fe929ff400a9f34b
YahyaNashar22 <cenations.people@live.com>
2023-10-30 16:14:25 +0200

    finished sign up form

--- client/src/App.js
+++ client/src/App.js
@@ -5,16 +5,18 @@ import Testimonials from "./components/Testimonials";
 import Location from "./components/Location";
 import Footer from "./components/Footer";
 import HomeFooter from "./components/HomeFooter";
+import SignUp from "./components/SignUp";
 
 function App() {
   return (
     <div>
-      <Service />
+      {/* <Service />
       <AboutUs />
       <Testimonials />
       <Location />
       <Footer />
-      <HomeFooter />
+      <HomeFooter /> */}
+      <SignUp />
     </div>
   );
 }
