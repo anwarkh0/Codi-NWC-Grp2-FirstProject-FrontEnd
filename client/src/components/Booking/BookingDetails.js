376b8ee3a4b4405e59ac383411fbb73d2ecc9888
YahyaNashar22 <cenations.people@live.com>
2023-11-06 15:56:53 +0200

    fixed styling

--- client/src/components/Booking/BookingDetails.js
+++ client/src/components/Booking/BookingDetails.js
@@ -1,38 +1,73 @@
-import React from 'react'
-import styleBooking from '../Booking/BookingDetails.module.css'
-const BookingDetails = () => {
+import React from "react";
+import styleBooking from "../Booking/BookingDetails.module.css";
+const BookingDetails = ({ close }) => {
   return (
-    <section className={styleBooking.container}>
-      <h1>RoomX</h1>
-      <h3>Your details</h3>
-      <form action='#' method='get'>
-      <section className={styleBooking.section}>
-        <label id='Name' name='fullName' className={styleBooking.label}>Full Name
-          <span className={styleBooking.asterisk}> *</span>
-        </label>
-        <p className={styleBooking.desc}>please give us the full name of the poeple stay at room</p>
-        <input required for='Name' type='text' className={styleBooking.inputField} />
-      </section>
+    <div className={styleBooking.whole}>
+      <section className={styleBooking.container}>
+        <h1>RoomX</h1>
+        <h3>Your details</h3>
+        <form action="#" method="get">
+          <section className={styleBooking.section}>
+            <label id="Name" name="fullName" className={styleBooking.label}>
+              Full Name
+              <span className={styleBooking.asterisk}> *</span>
+            </label>
+            <p className={styleBooking.desc}>
+              please give us the full name of the poeple stay at room
+            </p>
+            <input
+              required
+              for="Name"
+              type="text"
+              className={styleBooking.inputField}
+            />
+          </section>
 
-      <section className={styleBooking.section}>
-        <label id='Email' name='email' className={styleBooking.label}>Email address
-          <span className={styleBooking.asterisk}> *</span>
-        </label>
-        <p className={styleBooking.desc}>Your confirmation email goes here</p>
-        <input  required for='Email' type='email' className={styleBooking.inputField} />
-      </section>
+          <section className={styleBooking.section}>
+            <label id="Email" name="email" className={styleBooking.label}>
+              Email address
+              <span className={styleBooking.asterisk}> *</span>
+            </label>
+            <p className={styleBooking.desc}>
+              Your confirmation email goes here
+            </p>
+            <input
+              required
+              for="Email"
+              type="email"
+              className={styleBooking.inputField}
+            />
+          </section>
 
-      <section className={styleBooking.section}>
-        <label id='MobileNumber' className={styleBooking.label} name='MobileNumber'>Mobile Number
-          <span className={styleBooking.asterisk}> *</span>
-        </label>
-        <p className={styleBooking.desc}>We’ll only contact you in an emergency </p>
-        <input  required for='MobileNumber' type='text' className={styleBooking.inputField} />
+          <section className={styleBooking.section}>
+            <label
+              id="MobileNumber"
+              className={styleBooking.label}
+              name="MobileNumber"
+            >
+              Mobile Number
+              <span className={styleBooking.asterisk}> *</span>
+            </label>
+            <p className={styleBooking.desc}>
+              We’ll only contact you in an emergency{" "}
+            </p>
+            <input
+              required
+              for="MobileNumber"
+              type="text"
+              className={styleBooking.inputField}
+            />
+          </section>
+          <button type="submit" className={styleBooking.submitButton}>
+            Submit
+          </button>
+        </form>
+        <button className={styleBooking.closebtn} onClick={close}>
+          Close
+        </button>
       </section>
-      <button type='submit' className={styleBooking.submitButton}>Submit</button>
-</form>
-    </section>
-  )
-}
+    </div>
+  );
+};
 
-export default BookingDetails
+export default BookingDetails;
