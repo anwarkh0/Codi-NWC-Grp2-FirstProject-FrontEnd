b27a98edb871e648c41660c335130d7d2fb9537b
YahyaNashar22 <cenations.people@live.com>
2023-11-10 16:21:46 +0200

    sda

--- client/src/components/Hotels/Hotels.js
+++ client/src/components/Hotels/Hotels.js
@@ -14,9 +14,7 @@ const Hotels = () => {
     async function fetchData() {
       try {
         setIsloading(true);
-        const response = await axios.get(
-          "https://codi-nwc-grp2-firstproject-backend.onrender.com/hotel"
-        );
+        const response = await axios.get("http://localhost:8000/hotel");
         if (response) {
           setHotelData(response.data.data);
           // setIsloading(false)
