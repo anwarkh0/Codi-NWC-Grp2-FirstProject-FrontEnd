bb1a3a2107e6f34b79234b3b29cb06de1d33fc64
Layla Hammoud <laylahammoud63@gmail.com>
2023-12-30 13:39:31 +0200

    finsih search page and add conditonal rendering

--- client/src/components/Rooms/SelectedRooms.js
+++ client/src/components/Rooms/SelectedRooms.js
@@ -6,42 +6,32 @@ import { useState, useEffect } from "react";
 import { useLocation, Link } from "react-router-dom";
 import RoomCard from "../roomCard/RoomCard.js";
 import loadingImg from "../../assets/images/hotel-loading-gif.gif";
+import nodata from "../../assets/images/nodata.jpg";
 import UseApi from "../../hookes/useApi.js";
 
 function SelectedRooms({ idHotel }) {
   const location = useLocation();
-  const {apiCall,loading,error}=UseApi()
-  const [type, setType] = useState('default');
-  const [roomData, setRoomData] = useState(null);
+  const { apiCall, loading, error } = UseApi();
+  const [roomData, setRoomData] = useState([]);
   const select = location.state && location.state.select;
   // const [DefaultData, setDefaultData] = useState(false);
 
-//   useEffect(()=>{
-//     console.log(type)
-//     const fetchRooms =async () =>{
-//       try {
-//         const response = await apiCall({ url: "/room/order", method: "post" ,data:{type} })
-//         setRoomData(response.data)
-//       } catch (error) {
-//         console.error(error);
-//       }
-//     }
-//     fetchRooms()
-// },[type])
-  const [active, setActive] = useState(false);
-  const clickHandler = () => {
-    setActive(!active);
-  };
-
-  let ink = roomsModule.open;
-  let arr = down;
-  if (active) {
-    ink = roomsModule.open;
-    arr = up;
-  } else {
-    ink = roomsModule.closed;
-    arr = down;
-  }
+  useEffect(() => {
+    const fetchRooms = async () => {
+      try {
+        const response = await apiCall({
+          url: "/room/search",
+          method: "post",
+          data: { select },
+        });
+        setRoomData(response.data);
+      } catch (error) {
+        console.error(error);
+      }
+    };
+    fetchRooms();
+  }, [select]);
+  console.log(roomData);
 
   //creating sorting ref
   // const defaultSorting = useRef();
@@ -60,35 +50,46 @@ function SelectedRooms({ idHotel }) {
     <>
       <div className={roomsModule.wrapper}>
         <h1 className={roomsModule.header}>Selected Rooms Section</h1>
-        <p className={roomsModule.subHeader}>rooms result depends on your search specification</p>
-          {!loading && roomData ? (
-            <div className={roomsModule.gridView}>
-            {roomData.map((room, index) => {
-              return (
-                <RoomCard
-                  roomId={room.id}
-                  image={room.RoomImages[0]}
-                  quality={room.quality}
-                  hotel={room.hotel}
-                  price={room.price}
-                  key={index}
-                />
-                );
-              })}
-              </div>
-          ) : (
-            <span className={roomsModule.loading}>
-              <img
-                src={loadingImg}
-                style={{
-                  width: "15rem",
-                  height: "15rem",
-                }}
-                alt="loading"
+        <p className={roomsModule.subHeader}>
+          rooms result depends on your search specification
+        </p>
+        {!loading && roomData.length>0 ? (
+          <div className={roomsModule.gridView}>
+            {roomData.map((room, index) => (
+              <RoomCard
+                roomId={room.id}
+                image={room.RoomImages[0]}
+                quality={room.quality}
+                hotel={room.hotel}
+                price={room.price}
+                key={index}
               />
-            </span>
-          )}
-        </div>
+            ))}
+          </div>
+        ) : !loading && roomData.length === 0 ? (
+          <span className={roomsModule.loading}>
+          <img
+            src={nodata}
+            style={{
+              width: "15rem",
+              height: "15rem",
+            }}
+            alt="loading"
+          />
+        </span>
+        ) : (
+          <span className={roomsModule.loading}>
+            <img
+              src={loadingImg}
+              style={{
+                width: "15rem",
+                height: "15rem",
+              }}
+              alt="loading"
+            />
+          </span>
+        )}
+      </div>
     </>
   );
 }
